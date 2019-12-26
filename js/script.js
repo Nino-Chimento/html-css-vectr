2f3fe9faa797d8773b95b36b27be285ebca1f095
Nino Chimento <saninoc84@gmail.com>
2019-12-26 15:10:46 +0100

    menu xs appare con javascript

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var displayYes = document.getElementById('hamburgher-clik');
+displayYes.addEventListener("click",
+function(){
+  var apparizioneMenu = document.getElementById("menu-xs-apparizione");
+  apparizioneMenu.classList.remove("display-none-menu");
+}
+)
