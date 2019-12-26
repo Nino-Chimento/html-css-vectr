c4a00fce015584107b0d5400c13d032c64b6adce
Nino Chimento <saninoc84@gmail.com>
2019-12-26 16:00:58 +0100

    creazione variabili form

---
+++ js/script2.js
@@ -0,0 +1,9 @@
+var spedisci = document.getElementById('send');
+spedisci.addEventListener("click",
+function() {
+  var name = document.getElementById('first-name').value;
+  var surname = document.getElementById('second-name').value;
+
+
+}
+)
